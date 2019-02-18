f16fbd9a7a4ceab5b081f17b12d05ea5f3b1877b
jstolpe <stolpe.justin@gmail.com>
2019-02-18 11:20:51 -0600

    initial code commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<!-- title of our page -->
+		<title>Easy, Code Is</title>
+
+		<!-- include fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Coda" rel="stylesheet">
+
+		<!-- need this so everything looks good on mobile devices -->
+		<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+
+		<!-- css styles for our home page-->
+		<link href="css/home.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<div class="background-video-container">
+			<video class="background-video-element" autoplay muted loop >
+				<source src="assets/background_video.mp4" />
+			</video>
+			<img class="background-video-image" src="assets/background_video_image.png" />
+			<div class="background-video-overlay"></div>
+			<div class="background-video-text-overlay">
+				Easy, Code Is
+			</div>
+		</div>
+		<div class="content">
+			<div class="content-inner">
+				<div class="content-inner-padding">
+					<h1>
+						Welcome to Easy, Code Is!
+					</h1>
+					<div>
+						This is the website where you, the users, decide what features get added. Comment on any of my videos and let me know what you want to learn and see implemented! I will constantly be building on this website, adding features and creating videos based on what you, the users, want to see and learn.
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
