5fd7799bee96609cd3e26b36eedf823a508a7c69
U-DESKTOP-0FNSDVH\justi <stolpe.justin@gmail.com>
2020-04-22 17:50:12 -0500

    updates

--- autoloader.php
+++ autoloader.php
@@ -6,7 +6,7 @@
 	define( 'USER_LEVEL_ADMIN', '1' );
 
 	// include config (creds and things we keep out of www and repo)
-	include_once __DIR__ . '/../../easycodeis_includes/config.php';
+	include_once __DIR__ . ( PHP_OS == 'Linux' ? '' : '/' ) . '../../easycodeis_includes/config.php';
 
 	// include global functions
-	include_once __DIR__ . '/php/functions.php';
+	include_once __DIR__  . '/php/functions.php';
